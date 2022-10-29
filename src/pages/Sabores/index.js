b4367a98ea3404f95f4abb0d158ec1c1d99c645f
JhonatanSO <jhonerex2012@gmail.com>
2022-10-29 10:39:59 -0300

    Adc inicio projeto Sorveteria

---
+++ src/pages/Sabores/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sabores = () => (
+  <div className="Sabores">
+    <h1>Sabores</h1>
+  </div>
+)
+
+export default Sabores;
---
+++ src/pages/Sobre/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sobre = () => (
+  <div className="Sobre">
+    <h1>Sobre</h1>
+  </div>
+)
+
+export default Sobre;

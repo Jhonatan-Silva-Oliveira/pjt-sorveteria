77a79bc9a9e6cf19a9ef82364f69ad6d24e2dfe7
JhonatanSO <jhonerex2012@gmail.com>
2022-11-01 12:30:31 -0300

    adc estilo topo/conteudo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,36 +9,42 @@ const Home = () => (
     
     <main>
       <section className='secao-banner'>
-        <div>
+        <div className="limitar-secao box-secao-banner">
           <h1>Sorvete Artesanal</h1>
         </div>
       </section>
       
-      <section className='secao-sabores'>
-        <img src='../assets/banner-sabores.jpg'/>
+      <section className='secao-conteudo'>
+        <div className='box-secao'>
+          <img src='../assets/banner-sabores.jpg'/>
+        </div>
         
-        <div>
+        <div className='box-secao box-texto'>
           <h2>Nossos Sabores</h2>
           <span>Novos e deliciosos!</span>
           <p>Sorvete bom é aquele feito com os melhores ingredientes! Aqui na gelateria todos os nossos produtos são naturais, á base de frutas e sem nenhum conservante! Também temos opções sem lactose e sem açúcar. Venha conhecer e perceber que tem como o sorvete ser delicioso e saudável ao mesmo tempo!</p>
         </div>
       </section>
       
-      <section className='secao-eventos'>
-        <div>
+      <section className='secao-conteudo'>
+        <div className='box-secao box-texto'>
           <h2>Nossos Eventos</h2>
           <span>Delicias com sorvete!</span>
           <p>Mais do que uma sorveteria, uma extensão da sua casa! Estamos aqui prontinhos para te atender e oferecer os melhores eventos com os melhores sorvete da sua vida! Venha nos conhecer e passar um tempo aqui com a gente.</p>
         </div>
         
-        <img src='../assets/eventos-image.jpg'/>
+        <div className='box-secao'>
+          <img src='../assets/eventos-image.jpg'/>
+        </div>
       </section>
       
       
-      <section className='secao-sobre'>
-        <img src='../assets/sobre-image.jpg'/>
+      <section className='secao-conteudo'>
+        <div className='box-secao'>
+          <img src='../assets/sobre-image.jpg'/>
+        </div>
         
-        <div>
+        <div className='box-secao box-texto'>
           <h2>Sobre Nós</h2>
           <span>Alegria em cada casquinha!</span>
           <p>Venha tornar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melhor para o nosso cliente e você  não pode ficar fora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.</p>

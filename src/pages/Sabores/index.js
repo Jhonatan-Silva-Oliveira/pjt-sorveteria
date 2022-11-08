de93d481daaeddbf4f468418922663bb41e99558
JhonatanSO <jhonerex2012@gmail.com>
2022-11-08 15:10:51 -0300

    Adc pagina 'Sabores' ja com estilo e responsividade

--- src/pages/Sabores/index.js
+++ src/pages/Sabores/index.js
@@ -1,9 +1,72 @@
 import React from 'react';
+import Topo from '../../componentes/Topo';
+import Rodape from '../../componentes/Rodape'
 import './style.css';
 
 const Sabores = () => (
   <div className="Sabores">
-    <h1>Sabores</h1>
+    
+    <Topo />
+    
+    <main>
+      <section className="secao-banner-sabores">
+        <div className="box-secao-banner-sabores">
+          <h1>Nossos Sabores</h1>
+        </div>
+      </section>
+      
+      <section className="limitar-secao secao-cards-sabores">
+        <h2>Sabores de Sorvete</h2>
+        
+        <div className="container-cards">
+        
+          <div className="box-card">
+            <img src="./assets/sabor-oreo.png" />
+            <div>
+              <h3>Sorvete de Oreo</h3>
+              <p>Delicioso sorvete sabor Oreo. Uma explosão de sabor.</p>
+            </div>
+          </div>
+          <div className="box-card">
+            <img src="./assets/sabor-pistache.png" />
+            <div>
+              <h3>Sorvete Pistache</h3>
+              <p>Cremoso sorvete sabor pistache com pedacinhos de semente.</p>
+            </div>
+          </div>
+          <div className="box-card">
+            <img src="./assets/sabor-cookies-avela.png" />
+            <div>
+              <h3>Sorvete Cookies & Avelã</h3>
+              <p>O nosso melhor sorvete. Você vai adorar o sabor.</p>
+            </div>
+          </div>
+          <div className="box-card">
+            <img src="./assets/sorbet-kiwi.png" />
+            <div>
+              <h3>Sorvete de Kiwi</h3>
+              <p>Delicioso e refrescante sorvete sabor kiwi. Rico em vitamina C.</p>
+            </div>
+          </div>
+          <div className="box-card">
+            <img src="./assets/sorbet-morango.png" />
+            <div>
+              <h3>Sorvete de Morango</h3>
+              <p>Sorvete de morango gourmet. Tradicional e saboroso.</p>
+            </div>
+          </div>
+          <div className="box-card">
+            <img src="./assets/sorbet-limao.png" />
+            <div>
+              <h3>Sorvete de Limão Siciliano</h3>
+              <p>O incrível sorvete gourmet de limão siciliano vai te encantar.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    
+    <Rodape />
   </div>
 )
 
